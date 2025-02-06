0855d2a2fefbc5c4a945424945b34f5d8aae7ef4
CraftingDragon007 <craftingdragon007@outlook.com>
2025-02-06 16:06:04 +0100

    Now using HTTPExceptions in channels for faulty responses

--- routers/channels.py
+++ routers/channels.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from datetime import datetime, timezone
 from datahub import *
@@ -26,9 +26,9 @@ async def recent_channels_route():
 async def curve_data_route(channel_name: str, begin_time: int, end_time: int, backend: str = "sf-databuffer", num_bins: int = 0, query_expansion: bool = False):
     # If the channel name is a number interpret it as seriesId and don't check if it exists. Otherwise verify the channel name exists
     if not channel_name.isdigit() and not search_channels(channel_name.strip()):
-        return {"message": "Channel does not exist in backend"}, 400
+        raise HTTPException(status_code=404, detail="Channel does not exist in backend")
     if begin_time * end_time == 0:
-        return {"message": "begin_time or end_time is invalid, must be valid unix time (seconds)"}, 400
+        raise HTTPException(status_code=400, detail="begin_time or end_time is invalid, must be valid unix time (seconds)")
 
     query = {
         "channels": [channel_name],
@@ -47,6 +47,6 @@ async def curve_data_route(channel_name: str, begin_time: int, end_time: int, ba
             dataframe = table.as_dataframe()
             data = dataframe.to_dict(orient='index')
             curve[channel_name] = {timestamp: entry[channel_name] for timestamp, entry in data.items()}
-    except Exception as e:
-        return {"message": f"There was an error while fetching the data: {e}"}, 500
+    except Exception:
+        raise HTTPException(status_code=500, detail="Error fetching data from backend")
     return {"curve": curve}
