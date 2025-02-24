328caeb1b2509006eead69aebd511d04779da897
Erik Schwarz <erik.schwarz@psi.ch>
2025-02-24 10:50:25 +0100

    Updated timeouts in accordance with alexandre

--- routers/channels.py
+++ routers/channels.py
@@ -10,7 +10,7 @@ from shared_resources.datahub_synchronizer import search_channels, get_curve_dat
 router = APIRouter()
 
 @router.get("/search", tags=["channels"])
-@timeout(10)
+@timeout(15)
 def search_channels_route(search_text: str = ""):
     channels = []
     if search_text == "":
@@ -29,7 +29,7 @@ def recent_channels_route():
     return JSONResponse(content=result, status_code=200)
 
 @router.get("/curve", tags=["channels"])
-@timeout(10)
+@timeout(30)
 def curve_data_route(channel_name: str, begin_time: int, end_time: int, backend: str = "sf-databuffer", num_bins: int = 0, query_expansion: bool = False):
     entry = {}
     # If the channel name can be converted to an integer, treat it as seriesId.
