62370e515e4567f1bb276040779a64a9c347702c
Erik Schwarz <erik.schwarz@psi.ch>
2025-04-16 13:36:58 +0200

    Removed unused code

--- shared_resources/variables.py
+++ shared_resources/variables.py
@@ -5,14 +5,6 @@ from threading import Lock
 class SharedVariables:
     def __init__(self):
         self.redis_client = redis.StrictRedis(host=getenv("REDIS_HOST", "redis"), port=getenv("REDIS_PORT", 6379), db=0, decode_responses=True)
-        self.REDIS_IMAGE_STREAM = 'image_stream'
-
-        # Channels to fetch: channel is the key and the last accessed time (secssinceepoch) is the value
-        self.active_channels = {'random.1|TEST': 1e20, 'random.2|TEST': 1e20}
-        self.active_channels_lock = Lock()
-
-        self.channel_store_time_seconds = 3600
-        self.max_channel_frequency = 100
 
         # Recently accessed channels, doesnt invalidate, unlike active channels
         self.recent_channels = []
